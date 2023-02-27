ee48e15ee5e672b4a0b58e36c15bf0c0d64ad926
HadalyVillasclaras <hadalyvf@gmail.com>
2023-02-27 15:08:33 +0100

    fix: icon element

--- index.js
+++ index.js
@@ -2,52 +2,62 @@ const form = document.querySelector("form");
 form.addEventListener("submit", validateForm);
 
 function showValidation(input, message, err) {
-  let inputClass = err ? "errInput" : "successInput";
-  let iconClass = err ? "errIcon" : "successIcon";
+    let inputClass = err ? "errInput" : "successInput"; 
+    let iconClass = err ? "errIcon" : "successIcon"; 
+    const elementMsg = document.getElementById(input.id + 'Msg');
 
-  const icon = input.nextElementSibling;
-  const elementMsg = document.getElementById(input.id + "Msg");
+		if (input.nextElementSibling === null) {
+			let icon = document.createElement("span");
+			input.insertAdjacentElement("afterend", icon)
+		}
 
-  input.className = inputClass;
-  icon.className = iconClass;
-  elementMsg.textContent = message;
-  input.focus();
+		if (input.nextElementSibling){
+			input.nextElementSibling.className = iconClass;
+		}
+		
+		input.className=inputClass;
+    elementMsg.textContent = message;
+    input.focus();
 }
+
 
 function validateForm(event) {
-  event.preventDefault();
-  const emailRegExp =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    event.preventDefault();
+    const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const password = document.getElementById("pass");
-  const confPass = document.getElementById("confPass");
-  const inputs = [name, email, password, confPass];
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const password = document.getElementById("pass");
+    const confPass = document.getElementById("confPass");
+    const inputs = [name, email, password, confPass];
 
-  inputs.map((input) => {
-    let message = "";
-    let alertF = false;
-    let err = true;
+    inputs.map((input) => {
+        let message = "";
+        let alertF = false;
+        let err = true;
 
-    if (input.value == "") {
-      message = "Por favor, rellene este campo.";
-    } else if (input.id === "email" && !emailRegExp.test(input.value)) {
-      message = "Por favor, introduce un email válido.";
-    } else if (input.id === "pass" && input.value.length > 8) {
-      message = "La clave no puede tener más de 8 caracteres.";
-    } else if (input.id === "confPass" && password.value !== confPass.value) {
-      message = "Las claves no coinciden.";
-    } else {
-      err = false;
-      message = "";
-      alert = true;
-    }
+        if(input.value == ""){
+            message = "Por favor, rellene este campo."
+        } else if(input.id === "email" && !emailRegExp.test(input.value)) {
+            message = "Por favor, introduce un email válido.";
+        } else if (input.id === "pass" && input.value.length > 8) {
+            message = "La clave no puede tener más de 8 caracteres.";
+        } else if (input.id === 'confPass' && password.value !== confPass.value) {
+            message =  "Las claves no coinciden.";
+        }else{
+            err = false;
+            message = "";
+            alert = true;
+        }
 
-    showValidation(input, message, err);
+        showValidation(input, message, err);
 
-    if (alertF) {
-      alert("Formulario enviado satisfactoriamente");
-    }
-  });
+        if(alertF) {alert("Formulario enviado satisfactoriamente")}
+    })
+
+
+    
+
+
 }
+
