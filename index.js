9f618665da81c170328725ad5a3328a958b7d95c
HadalyVillasclaras <hadalyvf@gmail.com>
2023-02-27 13:56:33 +0100

    refactor js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", validateForm);
 
-function showValidation(input, message, iconClass, inputClass) {
+function showValidation(input, message, err) {
+    let inputClass = err ? "errInput" : "successInput"; 
+    let iconClass = err ? "errIcon" : "successIcon"; 
+
     const icon = input.nextElementSibling;
-    const inputMsg = input.id + 'Msg';
-    const elementMsg = document.getElementById(inputMsg);
+    const elementMsg = document.getElementById(input.id + 'Msg');
 
     input.className=inputClass;
     icon.className = iconClass;
@@ -14,54 +16,36 @@ function showValidation(input, message, iconClass, inputClass) {
 
 function validateForm(event) {
     event.preventDefault();
+    const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     const name = document.getElementById("name");
     const email = document.getElementById("email");
     const password = document.getElementById("pass");
     const confPass = document.getElementById("confPass");
-
     const inputs = [name, email, password, confPass];
 
-    const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
     inputs.map((input) => {
-        let inputClass = "errInput";
-        let iconClass = "errIcon";
         let message = "";
         let alertF = false;
+        let err = true;
 
         if(input.value == ""){
-            iconClass= "errIcon";
-            inputClass = "errInput";
             message = "Por favor, rellene este campo."
         } else if(input.id === "email" && !emailRegExp.test(input.value)) {
-            iconClass= "errIcon";
-            inputClass = "errInput";
             message = "Por favor, introduce un email válido.";
         } else if (input.id === "pass" && input.value.length > 8) {
-            iconClass= "errIcon";
-            inputClass = "errInput";
             message = "La clave no puede tener más de 8 caracteres.";
         } else if (input.id === 'confPass' && password.value !== confPass.value) {
-            iconClass= "errIcon";
-            inputClass = "errInput";
             message =  "Las claves no coinciden.";
         }else{
-            iconClass = "successIcon";
-            inputClass = "successInput";
+            err = false;
             message = "";
             alert = true;
-
         }
 
-        showValidation(input, message, iconClass, inputClass);
+        showValidation(input, message, err);
 
         if(alertF) {alert("Formulario enviado satisfactoriamente")}
     })
-
-
-    
-
-
 }
 
